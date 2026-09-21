78ec74a226b0cd399d88c698dfa261a88926e985
Audi1207 <24051214145@mhs.unesa.ac.id>
2026-09-21 11:07:20 +0700

    Update LoginScreen

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,396 +1,310 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-import 'login_screen.dart';
+import 'main_screen.dart';
 
-// WARNA
 const green = Color(0xFFD8F602);
 const grey = Color(0xFF3B3A3E);
 const black = Color(0xFF202226);
 
-// ===============================
-// PROFILE SCREEN
-// ===============================
-
-class ProfileScreen extends StatelessWidget {
-  const ProfileScreen({super.key});
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: black,
 
-      // HEADER
-      appBar: AppBar(
-        backgroundColor: black,
-        elevation: 0,
-        automaticallyImplyLeading: false,
-        title: Text(
-          'Profil',
-          style: GoogleFonts.poppins(
-            fontSize: 18,
-            fontWeight: FontWeight.bold,
-            color: Colors.white,
-          ),
-        ),
-        actions: [
-          IconButton(
-            onPressed: () {},
-            icon: const Icon(
-              Icons.settings_outlined,
-              color: Colors.white70,
-            ),
-          ),
-        ],
-      ),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          padding: const EdgeInsets.symmetric(horizontal: 17),
 
-      // ISI HALAMAN
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const SizedBox(height: 31),
 
-            // PROFIL
-            const ProfileHeader(),
+              // LOGO
+              Center(
+                child: Column(
+                  children: [
+                    Container(
+                      width: 90,
+                      height: 90,
+                      padding: const EdgeInsets.all(10),
 
-            const SizedBox(height: 20),
-
-            // STATISTIK
-            const Row(
-              children: [
-                Expanded(
-                  child: StatCard(
-                    number: '12',
-                    title: 'Ditonton',
-                  ),
-                ),
-                Expanded(
-                  child: StatCard(
-                    number: '4',
-                    title: 'Favorit',
-                  ),
-                ),
-                Expanded(
-                  child: StatCard(
-                    number: '3',
-                    title: 'Unduhan',
-                  ),
-                ),
-              ],
-            ),
-
-            const SizedBox(height: 20),
-
-            // MENU
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 11),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-
-                  const Text(
-                    'Akun Saya',
-                    style: TextStyle(
-                      fontSize: 9,
-                      color: Colors.white70,
-                    ),
-                  ),
-
-                  const SizedBox(height: 7),
-
-                  const MenuCard(
-                    icon: Icons.history,
-                    title: 'Riwayat Tonton',
-                    subtitle: '12 film ditonton',
-                  ),
-
-                  const MenuCard(
-                    icon: Icons.favorite_outline,
-                    title: 'Film Favorit',
-                    subtitle: '4 film disimpan',
-                  ),
-
-                  const MenuCard(
-                    icon: Icons.download_outlined,
-                    title: 'Unduhan Saya',
-                    subtitle: '3 film tersedia offline',
-                  ),
-
-                  const SizedBox(height: 15),
-
-                  const Text(
-                    'Pengaturan',
-                    style: TextStyle(
-                      fontSize: 9,
-                      color: Colors.white70,
-                    ),
-                  ),
-
-                  const SizedBox(height: 7),
-
-                  const MenuCard(
-                    icon: Icons.person_outline,
-                    title: 'Edit Profil',
-                  ),
-
-                  const MenuCard(
-                    icon: Icons.notifications_outlined,
-                    title: 'Notifikasi',
-                  ),
-
-                  const MenuCard(
-                    icon: Icons.language_outlined,
-                    title: 'Bahasa',
-                    subtitle: 'Indonesia',
-                  ),
-
-                  const MenuCard(
-                    icon: Icons.help_outline,
-                    title: 'Bantuan & FAQ',
-                  ),
-
-                  const SizedBox(height: 15),
-
-                  // TOMBOL KELUAR
-                  SizedBox(
-                    width: double.infinity,
-                    height: 45,
-                    child: OutlinedButton.icon(
-                      onPressed: () {
-                        Navigator.pushAndRemoveUntil(
-                          context,
-                          MaterialPageRoute(
-                            builder: (_) => const LoginScreen(),
-                          ),
-                          (route) => false,
-                        );
-                      },
-                      icon: const Icon(
-                        Icons.logout,
-                        color: green,
+                      decoration: BoxDecoration(
+                        color: grey,
+                        borderRadius: BorderRadius.circular(22),
                       ),
-                      label: const Text(
-                        'Keluar',
-                        style: TextStyle(
-                          color: green,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      style: OutlinedButton.styleFrom(
-                        side: const BorderSide(color: green),
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(10),
-                        ),
+
+                      child: Image.asset(
+                        'image/logo_tontonaja.png',
+                        fit: BoxFit.contain,
                       ),
                     ),
-                  ),
 
-                  const SizedBox(height: 25),
-                ],
+                    const SizedBox(height: 10),
+
+                    Text(
+                      'TontonAja',
+                      style: GoogleFonts.poppins(
+                        fontSize: 23,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ],
+                ),
               ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
 
-// ===============================
-// PROFILE HEADER
-// ===============================
+              const SizedBox(height: 34),
 
-class ProfileHeader extends StatelessWidget {
-  const ProfileHeader({super.key});
+              // JUDUL
+              Text(
+                'Selamat Datang',
+                style: GoogleFonts.poppins(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
+              ),
 
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: [
-        const SizedBox(height: 25),
+              Text(
+                'Masuk untuk melanjutkan',
+                style: GoogleFonts.poppins(
+                  fontSize: 11,
+                  color: Colors.white70,
+                ),
+              ),
 
-        // AVATAR
-        Container(
-          width: 73,
-          height: 73,
-          decoration: BoxDecoration(
-            color: green.withValues(alpha: 0.13),
-            shape: BoxShape.circle,
-          ),
-          alignment: Alignment.center,
-          child: const Text(
-            'BN',
-            style: TextStyle(
-              fontSize: 20,
-              fontWeight: FontWeight.bold,
-              color: green,
-            ),
-          ),
-        ),
+              const SizedBox(height: 26),
 
-        const SizedBox(height: 10),
+              // EMAIL
+              Text(
+                'Email',
+                style: GoogleFonts.poppins(
+                  fontSize: 10,
+                  color: Colors.white,
+                ),
+              ),
 
-        // NAMA
-        Text(
-          'Budi Nugroho',
-          style: GoogleFonts.poppins(
-            fontSize: 16,
-            fontWeight: FontWeight.bold,
-            color: Colors.white,
-          ),
-        ),
+              const SizedBox(height: 6),
 
-        const SizedBox(height: 2),
-
-        // EMAIL
-        Text(
-          'budi.nugroho@email.com',
-          style: GoogleFonts.poppins(
-            fontSize: 10,
-            color: Colors.white60,
-          ),
-        ),
-
-        const SizedBox(height: 25),
-      ],
-    );
-  }
-}
-
-// ===============================
-// STAT CARD
-// ===============================
-
-class StatCard extends StatelessWidget {
-  final String number;
-  final String title;
-
-  const StatCard({
-    super.key,
-    required this.number,
-    required this.title,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      height: 64,
-      margin: const EdgeInsets.symmetric(horizontal: 5),
-      decoration: BoxDecoration(
-        color: black,
-        borderRadius: BorderRadius.circular(11),
-        border: Border.all(
-          color: grey,
-          width: 0.5,
-        ),
-      ),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text(
-            number,
-            style: GoogleFonts.poppins(
-              fontSize: 17,
-              fontWeight: FontWeight.bold,
-              color: Colors.white,
-            ),
-          ),
-          Text(
-            title,
-            style: GoogleFonts.poppins(
-              fontSize: 9,
-              color: Colors.white60,
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-// ===============================
-// MENU CARD
-// ===============================
-
-class MenuCard extends StatelessWidget {
-  final IconData icon;
-  final String title;
-  final String? subtitle;
-
-  const MenuCard({
-    super.key,
-    required this.icon,
-    required this.title,
-    this.subtitle,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      height: subtitle == null ? 48 : 52,
-      margin: const EdgeInsets.only(bottom: 6),
-      padding: const EdgeInsets.symmetric(horizontal: 10),
-      decoration: BoxDecoration(
-        color: black,
-        borderRadius: BorderRadius.circular(11),
-        border: Border.all(
-          color: grey,
-          width: 0.5,
-        ),
-      ),
-      child: Row(
-        children: [
-
-          // ICON
-          Container(
-            width: 34,
-            height: 34,
-            decoration: BoxDecoration(
-              color: grey.withValues(alpha: 0.35),
-              borderRadius: BorderRadius.circular(9),
-            ),
-            child: Icon(
-              icon,
-              color: Colors.white60,
-              size: 16,
-            ),
-          ),
-
-          const SizedBox(width: 10),
-
-          // TEXT
-          Expanded(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  title,
-                  style: GoogleFonts.poppins(
-                    fontSize: 11,
-                    fontWeight: FontWeight.w600,
-                    color: Colors.white,
-                  ),
+              TextField(
+                style: GoogleFonts.poppins(
+                  fontSize: 12,
+                  color: Colors.white,
                 ),
 
-                if (subtitle != null)
-                  Text(
-                    subtitle!,
-                    style: GoogleFonts.poppins(
-                      fontSize: 8,
-                      color: Colors.white54,
+                decoration: InputDecoration(
+                  hintText: 'contoh@email.com',
+                  hintStyle: GoogleFonts.poppins(
+                    fontSize: 12,
+                    color: Colors.white54,
+                  ),
+
+                  prefixIcon: const Icon(
+                    Icons.mail_outline_rounded,
+                    color: grey,
+                    size: 17,
+                  ),
+
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(9),
+                    borderSide: const BorderSide(color: grey),
+                  ),
+
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(9),
+                    borderSide: const BorderSide(color: grey),
+                  ),
+
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(9),
+                    borderSide: const BorderSide(
+                      color: green,
+                      width: 1.5,
                     ),
                   ),
-              ],
-            ),
-          ),
+                ),
+              ),
 
-          // PANAH
-          const Icon(
-            Icons.chevron_right,
-            color: Colors.white38,
-            size: 17,
+              const SizedBox(height: 13),
+
+              // PASSWORD
+              Text(
+                'Password',
+                style: GoogleFonts.poppins(
+                  fontSize: 10,
+                  color: Colors.white,
+                ),
+              ),
+
+              const SizedBox(height: 6),
+
+              TextField(
+                obscureText: true,
+
+                style: GoogleFonts.poppins(
+                  fontSize: 12,
+                  color: Colors.white,
+                ),
+
+                decoration: InputDecoration(
+                  hintText: 'Masukkan password',
+                  hintStyle: GoogleFonts.poppins(
+                    fontSize: 12,
+                    color: Colors.white54,
+                  ),
+
+                  prefixIcon: const Icon(
+                    Icons.lock_outline_rounded,
+                    color: grey,
+                    size: 17,
+                  ),
+
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(9),
+                    borderSide: const BorderSide(color: grey),
+                  ),
+
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(9),
+                    borderSide: const BorderSide(color: grey),
+                  ),
+
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(9),
+                    borderSide: const BorderSide(
+                      color: green,
+                      width: 1.5,
+                    ),
+                  ),
+                ),
+              ),
+
+              const SizedBox(height: 13),
+
+              // LUPA PASSWORD
+              Align(
+                alignment: Alignment.centerRight,
+
+                child: Text(
+                  'Lupa Password?',
+                  style: GoogleFonts.poppins(
+                    fontSize: 9,
+                    fontWeight: FontWeight.w500,
+                    color: green,
+                  ),
+                ),
+              ),
+
+              const SizedBox(height: 31),
+
+              // MASUK
+              SizedBox(
+                width: double.infinity,
+                height: 45,
+
+                child: ElevatedButton(
+                  onPressed: () {
+                    Navigator.pushAndRemoveUntil(
+                      context,
+                      MaterialPageRoute(
+                        builder: (_) => const MainScreen(),
+                      ),
+                      (route) => false,
+                    );
+                  },
+
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: green,
+                    foregroundColor: black,
+                    elevation: 0,
+
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(11),
+                    ),
+                  ),
+
+                  child: Text(
+                    'Masuk',
+                    style: GoogleFonts.poppins(
+                      fontSize: 12,
+                      fontWeight: FontWeight.bold,
+                      color: black,
+                    ),
+                  ),
+                ),
+              ),
+
+              const SizedBox(height: 13),
+
+              // TAMU
+              SizedBox(
+                width: double.infinity,
+                height: 45,
+
+                child: OutlinedButton(
+                  onPressed: () {
+                    Navigator.pushAndRemoveUntil(
+                      context,
+                      MaterialPageRoute(
+                        builder: (_) => const MainScreen(),
+                      ),
+                      (route) => false,
+                    );
+                  },
+
+                  style: OutlinedButton.styleFrom(
+                    side: const BorderSide(color: grey),
+
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(11),
+                    ),
+                  ),
+
+                  child: Text(
+                    'Lanjut sebagai Tamu',
+                    style: GoogleFonts.poppins(
+                      fontSize: 12,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ),
+
+              const SizedBox(height: 26),
+
+              // DAFTAR
+              Center(
+                child: Text.rich(
+                  TextSpan(
+                    text: 'Belum punya akun? ',
+                    style: GoogleFonts.poppins(
+                      fontSize: 10,
+                      color: Colors.white70,
+                    ),
+
+                    children: [
+                      TextSpan(
+                        text: 'Daftar',
+                        style: GoogleFonts.poppins(
+                          fontSize: 10,
+                          fontWeight: FontWeight.bold,
+                          color: green,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+
+              const SizedBox(height: 30),
+            ],
           ),
-        ],
+        ),
       ),
     );
   }
